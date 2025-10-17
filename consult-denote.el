c3a8dccdcf375223036188ab7cacc0c6e421406d
Protesilaos Stavrou <info@protesilaos.com>
2025-10-17 09:36:40 +0300

    Update to consult-denote version 0.4.0

--- consult-denote.el
+++ consult-denote.el
@@ -5,7 +5,7 @@
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; Maintainer: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://github.com/protesilaos/consult-denote
-;; Version: 0.3.1
+;; Version: 0.4.0
 ;; Package-Requires: ((emacs "28.1") (denote "4.0.0") (consult "2.2"))
 
 ;; This file is NOT part of GNU Emacs.
